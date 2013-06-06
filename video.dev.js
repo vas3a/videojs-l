84dac3a67b92f8608d6a14bbbc8ee8dec1068525
Vasilica Olariu <vasilica.olariu@vuzum.com>
2013-06-06 13:59:14 +0300

    removed poster

--- video.dev.js
+++ video.dev.js
@@ -4037,50 +4037,6 @@ vjs.one(window, 'load', function(){
 // Run Auto-load players
 // You have to wait at least once in case this script is loaded after your video in the DOM (weird behavior only with minified version)
 vjs.autoSetupTimeout(1);
-/* Poster Image
-================================================================================ */
-/**
- * Poster image. Shows before the video plays.
- * @param {vjs.Player|Object} player
- * @param {Object=} options
- * @constructor
- */
-vjs.PosterImage = vjs.Button.extend({
-  /** @constructor */
-  init: function(player, options){
-    vjs.Button.call(this, player, options);
-
-    if (!player.poster() || !player.controls()) {
-      this.hide();
-    }
-
-    player.on('play', vjs.bind(this, this.hide));
-  }
-});
-
-vjs.PosterImage.prototype.createEl = function(){
-  var el = vjs.createEl('div', {
-        className: 'vjs-poster',
-
-        // Don't want poster to be tabbable.
-        tabIndex: -1
-      }),
-      poster = this.player_.poster();
-
-  if (poster) {
-    if ('backgroundSize' in el.style) {
-      el.style.backgroundImage = 'url("' + poster + '")';
-    } else {
-      el.appendChild(vjs.createEl('img', { src: poster }));
-    }
-  }
-
-  return el;
-};
-
-vjs.PosterImage.prototype.onClick = function(){
-  this.player_.play();
-};
 vjs.plugin = function(name, init){
   vjs.Player.prototype[name] = init;
 };
